cc149c444305c0180fe0af1a67817a924e5caf02
ollyjw <owilcocks@gmail.com>
2022-11-03 13:11:50 +0000

    added clear btn & click events for operators

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const display = document.querySelector('.display');
 const numbers = document.querySelectorAll('.btn.number');
 const operators = document.querySelectorAll('.btn.operator');
 const operatorsArray = Array.from(operators);
+const clearBtn = document.querySelector('#clear');
+const equalsBtn = document.querySelector('#equals');
+let result;
 
 
 function add(a, b) {
@@ -36,18 +39,47 @@ function operate(operator, num1, num2) {
     }
 }
 
+// need to store the full number (display value) before operator is pressed
+let displayValue = [];
+// save which operation has been chosen
+let operation = [];
 
 
-// Add click event listener to number btns that places the textContent of pressed btns on the display
+// Add click event listener to number btns that places the textContent of pressed btns on the display, then pushes it to displayValue array
 numbers.forEach(function (number) {
     number.addEventListener('click', () => {
         display.textContent += number.textContent;
+        displayValue[0] = display.textContent;
     })
 })
 
-// need to store the full number somewhere
-// save which operation has been chosen
+// Add click event listener to operator btns that places the textContent of pressed btns on the display & 'pushes' its value to operation var
+operators.forEach(function (operator) {
+    operator.addEventListener('click', () => {
+        display.textContent = operator.textContent;
+        operation[0] = display.textContent;
+    })
+})
+
+// Clear btn resets everything - empties vars/arrays
+function reset() {
+    display.textContent = '';
+    displayValue = [];
+    operation = [];
+    result = '';
+}
+
+clearBtn.addEventListener('click', () => {
+    reset();
+})
+
+
 // then operate() on them when the user presses the “=” key.
+
+// equalsBtn.addEventListener('click', () => {
+//     result = operate();
+//     display.textContent = result;
+// })
 
 // You should already have the code that can populate the display, so once operate() has been called, update the display with the ‘solution’ to the operation.
 
